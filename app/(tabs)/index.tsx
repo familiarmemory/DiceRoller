96175d9d50c880dc028f5807bbeb5d12c9b479a3
Tyler <156209798+familiarmemory@users.noreply.github.com>
2025-03-23 01:16:53 -0400

    Created dice roller with dynamic side count and dice count

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,21 @@
 import { Image, StyleSheet, Platform } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { DiceDisplay } from '@/components/DiceDisplay';
+import React from 'react';
+import Pressable from '@/components/ui/Pressable';
+import { roll } from '@/scripts/roller';
+import { Picker } from '@react-native-picker/picker';
+//import { num } from '@/components/DiceDisplay';
+
 
 export default function HomeScreen() {
+  var [num, onNumChange] = React.useState(1);
+  var [sides, onSidesChange] = React.useState(4);
+  var [dice, onDiceChange] = React.useState(1);
+  
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -15,40 +25,72 @@ export default function HomeScreen() {
           style={styles.reactLogo}
         />
       }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
+      <ThemedView style={styles.contentContainer}>
+
+        <ThemedView style={styles.displayContainer}>
+          {DiceDisplay(num)}
+        </ThemedView>
+        <ThemedView style={styles.stepContainer}>
+          <Pressable
+            style={styles.button} 
+            activeOpacity={0.5}
+            onPress= {() => {num = roll(sides, dice); onNumChange(num);}}>
+            
+            {/// function with database call here -> then redirect to account(?) page with user data
+            } 
+            <ThemedText style = {styles.defaultText}>
+              Roll
+            </ThemedText>
+
+
+          </Pressable>
+
+          <ThemedView style = {styles.pickerContainer}>
+              
+            <ThemedText style = {styles.defaultText}>
+              Number of Sides
+            </ThemedText>
+
+          
+            <Picker
+              selectedValue={sides}
+              onValueChange={(itemValue, itemIndex) =>
+                onSidesChange(itemValue)
+              }>
+              <Picker.Item label="4" value="4" />
+              <Picker.Item label="6" value="6" />
+              <Picker.Item label="8" value="8" />
+              <Picker.Item label="10" value="10" />
+              <Picker.Item label="12" value="12" />
+              <Picker.Item label="20" value="20" />
+              <Picker.Item label="100" value="100" />
+
+
+            </Picker>
+
+            <ThemedText style = {styles.defaultText}>
+              Number of Dice
+            </ThemedText>
+
+            <Picker 
+              selectedValue={dice}
+              onValueChange={(itemValue, itemIndex) =>
+                onDiceChange(itemValue)
+              }>
+              <Picker.Item label="1" value= "1" />
+              <Picker.Item label="2" value="2" />
+              <Picker.Item label="4" value="4" />
+              <Picker.Item label="6" value="6" />
+              <Picker.Item label="8" value="8" />
+              <Picker.Item label="10" value="10" />
+
+
+            </Picker>
+
+          </ThemedView>
+        </ThemedView>
+
+
       </ThemedView>
     </ParallaxScrollView>
   );
@@ -60,9 +102,38 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     gap: 8,
   },
+  contentContainer: {
+    //alignContent: 'center',
+    //justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,  
+    width: '100%',
+    height: '100%',
+  },
+  displayContainer: {
+    
+    width: 200,
+    height: 100,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+    padding: 8,
+    backgroundColor: 'transparent',
+    borderColor: '#D3D3D3',
+    borderWidth: 3
+  },
   stepContainer: {
     gap: 8,
     marginBottom: 8,
+  },
+  pickerContainer: {
+    //borderColor: '#D3D3D3',
+    //borderWidth: 3,
+    width: 300,
+    display: 'flex',
+    //flexDirection: 'row',
+    //gap: 8,
   },
   reactLogo: {
     height: 178,
@@ -70,5 +141,18 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     position: 'absolute',
+  },
+  button: {
+    backgroundColor: '#707070',
+    borderRadius: 20,
+    padding: 8,
+    margin: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  defaultText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
